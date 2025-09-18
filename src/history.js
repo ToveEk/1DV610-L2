9cc3f3fdb1c83f781df1b181aad569d269b2c00f
Tove Ek <te222uu@student.lnu.se>
2025-09-18 15:25:54 +0200

    Add initial structure for History class

--- src/history.js
+++ src/history.js
@@ -1,1 +1,13 @@
 // History tracking for dice rolls
+
+/**
+ * Class representing the history of previous dice rolls.
+ */
+export class History {
+  /**
+   * Gets previous dice rolls.
+   */
+  getPreviousRolls () {
+
+  }
+}
