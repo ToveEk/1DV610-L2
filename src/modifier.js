0e733eada2acdb6390b023ee125d5a31e8833aec
Tove Ek <te222uu@student.lnu.se>
2025-09-18 15:27:17 +0200

    Add initial structure for Modifier class

--- src/modifier.js
+++ src/modifier.js
@@ -1,2 +1,25 @@
 // Dice roll modifiers (ex: +2, -1)
 // Kopplas ihop med parser.js och rules.js?
+
+/**
+ * Class representing a modifier for dice rolls.
+ */
+export class Modifier {
+  /**
+   * Adds a specific number from a roll.
+   *
+   * @param modifier
+   */
+  addToRoll (modifier) {
+    // add a number to the dice roll
+  }
+
+  /**
+   * Removes a specific number from a roll.
+   *
+   * @param modifier
+   */
+  removeFromRoll (modifier) {
+
+  }
+}
