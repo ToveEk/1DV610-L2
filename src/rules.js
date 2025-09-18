4945e65fa95f2feda74937a4caea5a8eb22b7045
Tove Ek <te222uu@student.lnu.se>
2025-09-18 15:25:26 +0200

    Implement initial structure for Rules class

--- src/rules.js
+++ src/rules.js
@@ -1,2 +1,35 @@
 // Rules for dice rolling
 // Example: "advantage", "disadvantage", "keep highest", "keep lowest"
+
+/**
+ * Class representing different rules for dice rolling.
+ */
+export class Rules {
+  /**
+   * Rolls dice with an advantage.
+   */
+  rollWithAdvantage () {
+
+  }
+
+  /**
+   * Rolls dice with a disadvantage.
+   */
+  rollWithDisadvantage () {
+
+  }
+
+  /**
+   * Keeps the highest roll out of multiple dice rolls.
+   */
+  keepHighestRoll () {
+
+  }
+
+  /**
+   * Keeps the lowest roll out of multiple dice rolls.
+   */
+  keepLowestRoll () {
+
+  }
+}
