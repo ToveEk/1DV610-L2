fb1742017ec00e177cb8837ef729437e5a8550ff
Tove Ek <te222uu@student.lnu.se>
2025-09-19 14:45:12 +0200

    Refactor rollDie and rollDice methods to improve parsing logic

--- src/dice.js
+++ src/dice.js
@@ -14,20 +14,25 @@ import { Parser } from './parser.js'
  */
 export class Dice {
   diceArray = [4, 6, 8, 10, 12, 20, 100]
+  parser = new Parser()
 
   /**
    * Rolls a single die with the specified number of sides and returns the result message.
    *
-   * @param {number} sides - The number of sides on the die.
+   * @param {string} diceNotation - The dice notation string (e.g., "2d6").
    * @returns {string} - The result message or error message.
    */
-  rollDie (sides) {
+  rollDie (diceNotation) {
     try {
-      const parser = new Parser()
+      const parsedDice = this.parser.parseDice(diceNotation)
 
-      const parsedDice = parser.parseDice(sides)
+      console.log('The parsed dice:', parsedDice)
 
-      if (this.diceArray.includes(parsedDice)) {
+      if (parsedDice.numberOfDice > 1) {
+        return this.rollDice(diceNotation)
+      }
+
+      if (this.diceArray.includes(parsedDice.sides)) {
         const roll = Math.floor(Math.random() * parsedDice) + 1
         const result = `You rolled a ${roll} with a d${parsedDice}`
         return result
@@ -40,16 +45,31 @@ export class Dice {
     }
   }
 
-  // Handles rolling multiple dice
   /**
    * Rolls multiple dice with the specified number of sides.
    *
-   * @param {number} numDice - Number of dice to roll.
-   * @param {number} sides - Number of sides on each die.
+   * @param {string} diceNotation - The dice notation string (e.g., "2d6").
+   * @returns {string} - The result message or error message.
    */
-  rollDice (numDice, sides) {
-    // TODO: call rollDie in a loop the amount of numDice times
-    // example: rollDice(3, 6) -> rolls 3 six-sided dice
+  rollDice (diceNotation) {
+    try {
+      const parsedDice = this.parser.parseDice(diceNotation)
+
+      let roll = 0
+
+      if (this.diceArray.includes(parsedDice.sides)) {
+        for (let i = 0; i < parsedDice.numberOfDice; i++) {
+          roll += Math.floor(Math.random() * parsedDice.sides) + 1
+        }
+        const result = `You rolled a ${roll} with ${parsedDice.numberOfDice} d${parsedDice.sides}`
+        return result
+      } else {
+        const errorMessage = 'Type of dice not valid. Please choose one of the following:\nd4, d6, d8, d10, d12, d20 or d100'
+        return errorMessage
+      }
+    } catch (error) {
+      return 'An error occurred while rolling the dice.'
+    }
   }
 
   // TODO: add method for applying modifiers
