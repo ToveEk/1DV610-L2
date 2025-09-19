3d175e1974f58f117e1ce00393077dcc04ab4871
Tove Ek <te222uu@student.lnu.se>
2025-09-19 14:44:18 +0200

    Comment out logs for testing

--- src/testApp.js
+++ src/testApp.js
@@ -13,14 +13,16 @@ import { Dice } from './dice.js'
 
 const diceRoller = new Dice()
 
-console.log(diceRoller.rollDie('d4')) // Roll a single four-sided die
-console.log(diceRoller.rollDie('d6')) // Roll a single six-sided die
-console.log(diceRoller.rollDie('d8')) // Roll a single eight-sided die
-console.log(diceRoller.rollDie('d10')) // Roll a single ten-sided die
-console.log(diceRoller.rollDie('d12')) // Roll a single twelve-sided die
-console.log(diceRoller.rollDie('d20')) // Roll a single twenty-sided die
-console.log(diceRoller.rollDie('d100')) // Roll a single one-hundred-sided die
+// console.log(diceRoller.rollDie('d4')) // Roll a single four-sided die
 
-console.log(diceRoller.rollDie('d5')) // Invalid die type
-console.log(diceRoller.rollDie('6')) // Invalid notation
-console.log(diceRoller.rollDie()) // Invalid notation
+console.log(diceRoller.rollDie('2d6')) // Roll two six-sided dice
+console.log(diceRoller.rollDie('3d8')) // Roll three eight-sided dice
+
+// console.log(diceRoller.rollDie('d10')) // Roll a single ten-sided die
+// console.log(diceRoller.rollDie('d12')) // Roll a single twelve-sided die
+// console.log(diceRoller.rollDie('d20')) // Roll a single twenty-sided die
+// console.log(diceRoller.rollDie('d100')) // Roll a single one-hundred-sided die
+
+// console.log(diceRoller.rollDie('d5')) // Invalid die type
+// console.log(diceRoller.rollDie('6')) // Invalid notation
+// console.log(diceRoller.rollDie()) // Invalid notation
