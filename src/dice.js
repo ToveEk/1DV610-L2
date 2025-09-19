10aed57b317e9e3b718a3d872b66f92a412eff0e
Tove Ek <te222uu@student.lnu.se>
2025-09-19 16:59:51 +0200

    Add a roll method that handles rolling both single and multiple dice

--- src/dice.js
+++ src/dice.js
@@ -17,6 +17,41 @@ export class Dice {
   parser = new Parser()
 
   /**
+   * Rolls dice based on the provided dice notation string and returns the result message.
+   *
+   * @param {string} diceNotation - The dice notation string (e.g., "2d6").
+   * @returns {string} - The result message or error message.
+   */
+  roll (diceNotation) {
+    try {
+      const parsedDice = this.parser.parseDice(diceNotation)
+
+      console.log('The parsed dice:', parsedDice)
+
+      let roll = 0
+
+      // inte clean code lol
+      if (this.diceArray.includes(parsedDice.sides)) {
+        if (parsedDice.numberOfDice > 1) {
+          for (let i = 0; i < parsedDice.numberOfDice; i++) {
+            roll += Math.floor(Math.random() * parsedDice.sides) + 1
+          }
+        } else {
+          roll = Math.floor(Math.random() * parsedDice.sides) + 1
+        }
+
+        roll += parsedDice.modifier
+        const result = `You rolled a ${roll} with ${parsedDice.numberOfDice} d${parsedDice.sides}`
+        return result
+      } else {
+        return 'Type of dice not valid. Please choose one of the following:\nd4, d6, d8, d10, d12, d20 or d100'
+      }
+    } catch (error) {
+      return 'An error occurred while rolling the dice.'
+    }
+  }
+
+  /**
    * Rolls a single die with the specified number of sides and returns the result message.
    *
    * @param {string} diceNotation - The dice notation string (e.g., "2d6").
@@ -30,15 +65,20 @@ export class Dice {
 
       if (parsedDice.numberOfDice > 1) {
         return this.rollDice(diceNotation)
-      }
-
-      if (this.diceArray.includes(parsedDice.sides)) {
-        const roll = Math.floor(Math.random() * parsedDice) + 1
-        const result = `You rolled a ${roll} with a d${parsedDice}`
-        return result
       } else {
-        const errorMessage = 'Type of dice not valid. Please choose one of the following:\nd4, d6, d8, d10, d12, d20 or d100'
-        return errorMessage
+        let roll = 0
+
+        if (this.diceArray.includes(parsedDice.sides)) {
+          roll = Math.floor(Math.random() * parsedDice.sides) + 1
+
+          roll += parsedDice.modifier
+
+          const result = `You rolled a ${roll} with a d${parsedDice}`
+          return result
+        } else {
+          const errorMessage = 'Type of dice not valid. Please choose one of the following:\nd4, d6, d8, d10, d12, d20 or d100'
+          return errorMessage
+        }
       }
     } catch (error) {
       return 'An error occurred while rolling the die.'
@@ -59,8 +99,11 @@ export class Dice {
 
       if (this.diceArray.includes(parsedDice.sides)) {
         for (let i = 0; i < parsedDice.numberOfDice; i++) {
-          roll += Math.floor(Math.random() * parsedDice.sides) + 1
+          roll = Math.floor(Math.random() * parsedDice.sides) + 1
         }
+
+        roll += parsedDice.modifier
+
         const result = `You rolled a ${roll} with ${parsedDice.numberOfDice} d${parsedDice.sides}`
         return result
       } else {
@@ -72,15 +115,14 @@ export class Dice {
     }
   }
 
-  // TODO: add method for applying modifiers
+  // Hade kunnat göra rollDie och rollDice till en metod...
+  // nu blir det ju mycket kaka på kaka och bryter mot DRY
 
   // En metod för att enbart anropa parseDice?
 
   // En metod för att avgöra om en eller flera tärningar ska rullas? Därefter anropa lämplig metod?
 
   // En metod för att avgöra om regler ska tillämpas?
-
-  // En metod för att avgöra om modifieringar ska tillämpas?
 
   // Dice rolls behöver lagras någonstans... hm
 }
