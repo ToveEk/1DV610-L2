2d7de00086f0427bc5c88ef759524e22fcd06dae
Tove Ek <te222uu@student.lnu.se>
2025-09-23 17:59:36 +0200

    Update testApp.js to replace roll() method calls with startRolling() for consistency

--- src/testApp.js
+++ src/testApp.js
@@ -13,13 +13,16 @@ import { Dice } from './dice.js'
 
 const diceRoller = new Dice()
 
-console.log(diceRoller.roll('d4+5')) // Roll a single four-sided die with plus 5 modifier
-console.log(diceRoller.roll('2d6+1')) // Roll two six-sided dice with plus 1 modifier
-console.log(diceRoller.roll('3d8-2')) // Roll three eight-sided dice with minus 2 modifier
+// console.log(diceRoller.startRolling('d4+5')) // Roll a single four-sided die with plus 5 modifier
+// console.log(diceRoller.startRolling('2d6+1')) // Roll two six-sided dice with plus 1 modifier
+// console.log(diceRoller.startRolling('3d8-2')) // Roll three eight-sided dice with minus 2 modifier
 
 // console.log(diceRoller.rollDie('d10')) // Roll a single ten-sided die
 // console.log(diceRoller.rollDie('d12')) // Roll a single twelve-sided die
-// console.log(diceRoller.rollDie('d20')) // Roll a single twenty-sided die
+
+// console.log(diceRoller.startRolling('d20')) // Roll a single twenty-sided die
+console.log(diceRoller.startRolling('2d20 with advantage')) // Roll two twenty-sided dice
+
 // console.log(diceRoller.rollDie('d100')) // Roll a single one-hundred-sided die
 
 // console.log(diceRoller.rollDie('d5')) // Invalid die type
