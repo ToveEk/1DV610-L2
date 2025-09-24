2a37cbf07d9368b1234697f9a1338543f1586886
Tove Ek <te222uu@student.lnu.se>
2025-09-24 15:15:12 +0200

    Refactor testApp.js to test negative modifier

--- src/testApp.js
+++ src/testApp.js
@@ -15,15 +15,15 @@ const diceRoller = new Dice()
 
 // console.log(diceRoller.startRolling('d4+5')) // Roll a single four-sided die with plus 5 modifier
 // console.log(diceRoller.startRolling('2d6+1')) // Roll two six-sided dice with plus 1 modifier
-// console.log(diceRoller.startRolling('3d8-2')) // Roll three eight-sided dice with minus 2 modifier
+console.log(diceRoller.startRolling('3d8-2')) // Roll three eight-sided dice with minus 2 modifier
 
-// console.log(diceRoller.rollDie('d10')) // Roll a single ten-sided die
-// console.log(diceRoller.rollDie('d12')) // Roll a single twelve-sided die
+// console.log(diceRoller.startRolling('d10')) // Roll a single ten-sided die
+// console.log(diceRoller.startRolling('d12')) // Roll a single twelve-sided die
 
-console.log(diceRoller.startRolling('2d20 with advantage')) // Roll two twenty-sided dice with advantage
+// console.log(diceRoller.startRolling('2d20 with advantage')) // Roll two twenty-sided dice with advantage
 
-// console.log(diceRoller.rollDie('d100')) // Roll a single one-hundred-sided die
+// console.log(diceRoller.startRolling('d100')) // Roll a single one-hundred-sided die
 
-// console.log(diceRoller.rollDie('d5')) // Invalid die type
-// console.log(diceRoller.rollDie('6')) // Invalid notation
-// console.log(diceRoller.rollDie()) // Invalid notation
+// console.log(diceRoller.startRolling('d5')) // Invalid die type
+// console.log(diceRoller.startRolling('6')) // Invalid notation
+// console.log(diceRoller.startRolling()) // Invalid notation
